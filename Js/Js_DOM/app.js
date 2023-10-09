4313faf510b8ab3424494ea91ab5b5cfe927d391
vikash-pandey1 <vikashpandey7082@gmail.com>
2023-10-09 23:21:36 +0530

    initial commit

---
+++ Js/Js_DOM/app.js
@@ -0,0 +1,7 @@
+let a = document.getElementById("mainImg");
+console.dir(a);
+console.dir(document.getElementById("description"));
+console.log(document.getElementsByClassName("oldImg"));
+for(let i =0;i<smallImages.length;i++){
+    console.dir(smallImages[i].scr);
+}
